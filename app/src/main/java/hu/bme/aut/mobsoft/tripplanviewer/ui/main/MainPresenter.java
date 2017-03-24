1e10057163138e3e988722c6dfcd38ec6725f06f
Tamas <resitomi@gmail.com>
2017-03-24 09:50:15 +0100

    mvp implementation for main screen

---
+++ app/src/main/java/hu/bme/aut/mobsoft/tripplanviewer/ui/main/MainPresenter.java
@@ -0,0 +1,29 @@
+package hu.bme.aut.mobsoft.tripplanviewer.ui.main;
+
+import hu.bme.aut.mobsoft.tripplanviewer.ui.Presenter;
+
+
+public class MainPresenter extends Presenter<MainScreen> {
+
+    private static MainPresenter instance = null;
+
+    private MainPresenter() {
+    }
+
+    public static MainPresenter getInstance() {
+        if (instance == null) {
+            instance = new MainPresenter();
+        }
+        return instance;
+    }
+
+    @Override
+    public void attachScreen(MainScreen screen) {
+        super.attachScreen(screen);
+    }
+
+    @Override
+    public void detachScreen() {
+        super.detachScreen();
+    }
+}
---
+++ app/src/main/java/hu/bme/aut/mobsoft/tripplanviewer/ui/main/MainScreen.java
@@ -0,0 +1,6 @@
+package hu.bme.aut.mobsoft.tripplanviewer.ui.main;
+
+
+public interface MainScreen {
+    void showMessage(String text);
+}
